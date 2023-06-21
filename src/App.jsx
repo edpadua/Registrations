c971a25b856c0fd0d08f03e838bdc26b867cd471
edpadua <ed.padua@gmail.com>
2023-06-21 15:15:51 -0300

    Layout corrections
    Layout corrections

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@ function App() {
 
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className='w-full  bg-grey-300'>
+        <div className="font-bold text-5xl text-center pt-20 pb-20 bg-blue-500 text-white">
+          <h1>Cadastro de candidato</h1>
+        </div>
         <LoginForm />
       </div>
-      <Footer/>
+      <Footer />
     </>
 
   )
--- src/Components/LoginForm/index.jsx
+++ src/Components/LoginForm/index.jsx
@@ -52,6 +52,7 @@ function LoginForm() {
 
     return (
         <div className='mx-auto relative px-40 top-20 pb-40 block'>
+            
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
