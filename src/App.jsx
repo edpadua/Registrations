5f04cdbc2da3c695008c683ac82c604c35eabdc5
edpadua <ed.padua@gmail.com>
2023-06-21 09:18:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <NavBar/>
-      <div className='w-screen h-screen bg-grey-300'>
+      <div className='h-screen bg-grey-300'>
         <LoginForm />
       </div>
     </>
